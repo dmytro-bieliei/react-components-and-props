902f700f98fff9439a63e4ba182691c6f4f9dcad
dmytro-bieliei <dmbieliei@gmail.com>
2022-11-09 22:13:10 +0200

    fix mistakes

--- src/components/first goad/index.jsx
+++ src/components/first goad/index.jsx
@@ -1,15 +1,16 @@
- import { Component } from "react";
+import { Component } from "react";
 
- class Sneakers extends Component {
+class Sneakers extends Component {
   render() {
-    const { name, description, price, quantity } = this.props;
-    return ( 
+    const { goads: name, description, price, quantity } = this.props;
+    return (
       <article>
         <h3>Name</h3>
         <p>Description</p>
         <p>Price</p>
         <p>Amount</p>
       </article>
-    )
+    );
   }
- }
+}
+export default Sneakers
